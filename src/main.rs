ff297c72f161d0980df4dde31505f5c74106eefd
luannndev <106454269+luannndev@users.noreply.github.com>
2024-10-18 22:36:20 +0200

    Add .gitignore files and initial Cargo.lock
    Added `.gitignore` files to specify directories and files to be ignored by Git. Introduced `Cargo.lock` to track the exact versions of dependencies in use, ensuring consistent builds.

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use tracing::{debug, level_filters::LevelFilter};
+use tracing_subscriber::EnvFilter;
+
+mod backend;
+mod ball;
+mod game;
+mod paddle;
+mod physics;
+mod server;
+
+#[tokio::main]
+async fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+    init_tracing()?;
+    let mut server = server::AppServer::new();
+    server.run().await?;
+    Ok(())
+}
+
+fn init_tracing() -> color_eyre::Result<()> {
+    let filter = EnvFilter::builder()
+        .with_default_directive(LevelFilter::INFO.into())
+        .from_env()?
+        .add_directive("pong_russh=debug".parse()?);
+    tracing_subscriber::fmt()
+        .compact()
+        .with_env_filter(filter)
+        .init();
+    debug!("Tracing initialized");
+    Ok(())
+}
